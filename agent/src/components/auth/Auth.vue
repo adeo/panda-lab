a3df2aa19ab6934ced16754e7a0da9825f1e47e6
Romain Pamart <romain.pamart@yahoo.fr>
2019-08-21 15:15:07 +0200

    feat(User): add user and modify functions for add claims

---
+++ agent/src/components/auth/Auth.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="firebaseui-auth-container"></div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import * as firebase from "firebase";
+    import * as firebaseui from 'firebaseui'
+
+    @Component
+    export default class Auth extends Vue {
+        mounted() {
+            let uiConfig = {
+                signInSuccessUrl: '/auth/success',
+                signInOptions: [
+                    {
+                        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                        authMethod: 'https://accounts.google.com',
+                        clientId: '24857120470-lpj1m345qh0spg0jjfr379dumpf7r96j.apps.googleusercontent.com'
+                    },
+                    firebase.auth.EmailAuthProvider.PROVIDER_ID
+                ]
+            };
+            let ui = new firebaseui.auth.AuthUI(firebase.auth());
+            ui.start('#firebaseui-auth-container', uiConfig);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- agent/src/routers/router.ts
+++ agent/src/routers/router.ts
@@ -6,11 +6,11 @@ import Home from "@/components/Home.vue";
 import Website from "@/components/Website.vue";
 import ListDevice from "@/components/ListDevice.vue";
 import Jobs from "@/components/Jobs.vue";
-import Devices from "@/components/Devices.vue";
 import Device from "@/components/Device.vue";
 import Applications from "@/components/Applications.vue";
 import Job from "@/components/Job.vue";
 import JobTaskDetail from "@/components/JobTaskDetail.vue";
+import Auth from "@/components/auth/Auth.vue";
 
 Vue.use(VueRouter);
 
@@ -26,7 +26,7 @@ const HOME_ROUTE: RouteConfig = {
     component: Home,
     beforeEnter: SplashConfiguredGuard,
     children: [
-        {path: '', component: Website,},
+        {path: '', component: Auth,},
         {path: '/listDevice', component: ListDevice,},
         {path: '/jobs', component: Jobs,},
         {path: '/jobs/:jobId', component: Job},
@@ -34,6 +34,7 @@ const HOME_ROUTE: RouteConfig = {
         {path: '/phone', component: Jobs,},
         {path: '/devices/:deviceId', component: Device,},
         {path: '/applications', component: Applications,},
+        {path: '/auth', component: Auth,}
     ]
 };
 
